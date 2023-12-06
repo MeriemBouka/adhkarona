398853ce1a42637c905934ee5806405e483c1364
Meriem Timdouine <meriem.boukaddouss@gmail.com>
2023-12-06 13:06:00 +0100

    Supression du champ bleu au clique

--- src/utils/GlobalStyle.jsx
+++ src/utils/GlobalStyle.jsx
@@ -7,10 +7,16 @@ body{
     box-sizing: border-box;
     direction: rtl;
 }
+button:active,
+li:active,
+a:active {
+  -webkit-tap-highlight-color: transparent !important;
+}
 *{
     background-color : ${(props) => (props.isDarkMode ? "#202124" : "#153c2d")};
     color : ${(props) => (props.isDarkMode ? "#a18855" : "#ffe6a8")};
     font-family: "Noto Naskh Arabic";
+    
 }
 
 `;
