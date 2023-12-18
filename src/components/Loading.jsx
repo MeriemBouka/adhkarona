bd5ff8fbf20e5324d33bb8cff9be351a99f4935f
Meriem Timdouine <meriem.boukaddouss@gmail.com>
2023-12-18 20:13:43 +0100

    loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,35 @@
+import styled, { keyframes } from 'styled-components';
+import verset from "../assets/verset-min.png";
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+`;
+
+const StyledImage = styled.img`
+ width:100%;
+  animation: ${fadeOut} 2.5s linear forwards;
+`;
+
+const Loading = ({ opacity }) => {
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems:"center",
+        opacity,
+        height:"100vh",
+        display: opacity === 1 ? "flex" : "none",
+      }}
+    >
+      <StyledImage alt="verset" src={verset} />
+    </div>
+  );
+};
+
+export default Loading;
